ba439414fee8541169526945bbeea067ecc1ba5a
nblxanueb <a.e.pykhantsev@gmail.com>
2018-03-16 03:30:30 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -36,3 +36,9 @@ function removeElementFromBeginningOfArray(array) {
   return array.slice(1);
 
 }
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop();
+  return array;
+
+}
